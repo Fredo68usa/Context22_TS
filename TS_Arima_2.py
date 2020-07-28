dbbe2d8136d2e2a874c294de3e3544ba928cef7e
Fredo68 <fredo68@att.net>
2020-07-28 14:35:43 +0000

    First Commit

--- TS_Arima_2.py
+++ TS_Arima_2.py
@@ -3,27 +3,34 @@ import datetime as dt
 from matplotlib import pyplot
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
+import numpy as np
+import math
+
 
 def parser(x):
-        date1=dt.datetime.strptime(x[:19], '%Y-%m-%dT%H:%M:%S')
-        # print (date1)
-        date2=date1.timestamp()
-        # print (date2)
-        return (date1)
+          print ( ' x = ' , ' - ', x )
+          date1=dt.datetime.strptime(x[:19], '%Y-%m-%dT%H:%M:%S')
+          # print (date1)
+          # date2=date1.timestamp()
+          # print (date1,date2)
+          return (date1)
         # return dt.datetime.strptime(x[:19], '%Y-%m-%dT%H:%M:%S')
 
-wd = '/opt/sonarg/sonarw/agg_out/'
+# wd = '/opt/sonarg/sonarw/agg_out/'
+wd = './'
 
 queri = input('Enter queri number : ')
 case = input('Enter case 1(working week)  or 2(sunday) : ')
 
-series = read_csv(wd + 'TS_Query_1.'+case+'.csv', index_col=1, parse_dates=[1],usecols = ['Timestamp','Records Affected'], date_parser=parser)
+series = read_csv(wd + 'TS_Query_' + queri + '.' + case +'.csv', index_col=1, parse_dates=[1],usecols = ['Timestamp','Records Affected'], date_parser=parser)
 print('Series ',series.head())
 print(series.shape)
 
 # Data Cleaning
 series_clean = series.dropna()
 X=series_clean.values
+print ('Null ' , series_clean.isnull().sum())
+
 
 size = int(len(X) * 0.66)
 train, test = X[0:size], X[size:len(X)]
@@ -43,7 +50,7 @@ for t in range(len(test)):
     delta = yhat-obs
     percent = (delta/obs)/100
     print ('predicted=%f, expected=%f , delta= %f , percent= %f' % (yhat, obs,delta, percent))
-    print (len(predictions), "" , len(history))
+    # print (len(predictions), "" , len(history))
 
 
 error = mean_squared_error(test, predictions)
@@ -54,7 +61,7 @@ print('Test MSE: %.3f' % error)
 pyplot.plot(test)
 pyplot.plot(predictions, color='red')
 pyplot.show()
-pyplot.savefig('Predictions'+case+'.png')
+pyplot.savefig('Predictions'+ queri + '.' + case +'.png')
 
 print ('=========================')
 print('Done')
